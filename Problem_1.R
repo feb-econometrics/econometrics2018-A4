ccdfa5a2679af60ff11d983fdcac2fd88b6b97eb
Boyko Amarov <boyko.amarov@gmail.com>
2018-06-01 14:02:49 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,2 +1,2 @@
-adv <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/sales.csv')
-str(adv)
+sales <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/sales.csv')
+str(sales)
