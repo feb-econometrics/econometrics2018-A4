766fc4511a2d7b8defd752019f0603bec5bf80de
Boyko Amarov <boyko.amarov@gmail.com>
2018-06-04 15:06:37 +0300

    Update Problem_2.R

--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,14 @@
 adv <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/Advertising.csv')
+
+## Fit separete regressio models for each advertising channel
+
+fitTV <- lm(sales ~ TV, data = adv)
+fitRadio <- lm(sales ~ radio, data = adv)
+fitNewspaper <- lm(sales ~ newspaper, data = adv)
+
+## Examine the estimated models
+
+summary(fitTV)
+summary(fitRadio)
+summary(fitNewspaper)
+
